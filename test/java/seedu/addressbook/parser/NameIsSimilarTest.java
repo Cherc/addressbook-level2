7c0a103d89a668db001b321e5db21f323cf3d17a
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-09-04 23:32:01 +0800

    Add first test null_returnsFalse()

--- test/java/seedu/addressbook/parser/NameIsSimilarTest.java
+++ test/java/seedu/addressbook/parser/NameIsSimilarTest.java
@@ -1,5 +1,6 @@
 package seedu.addressbook.parser;
 
+import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.*;
 
 import org.junit.Before;
@@ -8,7 +9,17 @@ import static org.junit.Assert.*;
 
 public class NameIsSimilarTest {
 
+    Name name1;
     
+    @Before 
+    public void setup() throws IllegalValueException{
+        name1 = new Name("John");
+    }
+    
+    @Test
+    public void null_returnsFalse(){
+        assertFalse(name1.isSimilar(null));
+    }
     
     
     
