f6d886b194fa62f8f6f999fbaa97ae6ae077fd10
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-09-04 19:37:00 +0800

    add sequenceNumber and nextSequenceNumber to Person class

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -10,10 +10,13 @@ import java.util.Objects;
  */
 public class Person implements ReadOnlyPerson {
 
+    static private int nextSequenceNumber = 0;
+    
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private int sequenceNumber;
 
     private final UniqueTagList tags;
     /**
@@ -25,6 +28,9 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.nextSequenceNumber = Person.nextSequenceNumber;
+        Person.nextSequenceNumber++;
+        
     }
 
     /**
