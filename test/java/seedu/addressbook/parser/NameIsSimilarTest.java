e7c8c774880953eba85e79f59d2e2a59fcba3c16
Cherlynn\Cherlynn <cherlynn@live.com.sg>
2016-09-04 23:27:17 +0800

    Add empty method isSimilar() to Name class. Add empty test for isSimilar() method.

---
+++ test/java/seedu/addressbook/parser/NameIsSimilarTest.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.parser;
+
+import seedu.addressbook.data.person.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class NameIsSimilarTest {
+
+    
+    
+    
+    
+}
